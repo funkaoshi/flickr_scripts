c8f955b5b6790d0a004a8c53aa71e0e8633c1803
Ramanan Sivaranjan <ramanan@funkaoshi.com>
2010-03-03 12:01:25 -0500

    A script to append some text to the end of a description.

---
+++ update_description.rb
@@ -0,0 +1,25 @@
+require 'flickraw'
+require 'flickr_auth.rb'
+
+# todo: read these from command line or something
+DESCRIPTION = 'DESCRIPTION'
+SET_ID = 'SET'
+
+# authorize at flickr, loading auth data from yaml file.
+auth = FlickrAuth.new('auth.yml', 'write')
+
+# get photos in set 
+photos = flickr.photosets.getPhotos(:photoset_id => SET_ID, :extras => 'description')
+
+photos.photo.each do |photo|
+  description = photo.description
+  description += "\n\n" unless description.empty?
+  description += DESCRIPTION
+  puts "Update: #{photo.title} - #{photo.id}"
+  
+  flickr.photos.setMeta(
+      :photo_id => photo.id,
+      :title => photo.title,
+      :description => description)
+end
+
